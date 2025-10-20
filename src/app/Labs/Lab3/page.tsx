5d62a78e41d20e3b5e27f0cc01ff76ec6402ff09
Vedant3008 <shahh7107@gmail.com>
2025-10-20 11:03:58 -0400

    Assignment 3

--- src/app/Labs/Lab3/page.tsx
+++ src/app/Labs/Lab3/page.tsx
@@ -1,8 +1,50 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import TernaryOperator from "./TernaryOperator";
+import IfElse from "./IfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions"
+import ImpliedReturn from "./ImpliedReturn";
+import TemplateLiterals from "./TemplateLiterals";
+import SimpleArrays from "./SimpleArrays";  
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
+import FilterFunction from "./FilterFunction";
+import House from "./House";
+import JsonStringify from "./JsonStringify";
+import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+
+
 export default function Lab3() {
-  return (
-    <div>
-      <h2>Lab 3</h2>
-      <p>This will be Lab 3.</p>
+  return(
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
+      <VariablesAndConstants/>
+      <VariableTypes/>
+      <TernaryOperator/>
+      <IfElse/>
+      <ConditionalOutputInline/>
+      <LegacyFunctions/>
+      <ArrowFunctions/>
+      <ImpliedReturn/>
+      <TemplateLiterals/>
+      <SimpleArrays/>
+      <ArrayIndexAndLength/>
+      <AddingAndRemovingToFromArrays/>
+      <ForLoops/>
+      <MapFunction/>
+      <FindFunction/>
+      <FilterFunction/>
+      <House/>
+      <JsonStringify/>
+      <Spreading/>
+      <Destructing/>
+      <FunctionDestructing/>
     </div>
-  );
-}
+);}
