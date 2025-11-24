2f689bb09ff2e87d98dc8c4c5048d90164c43644
Vedant3008 <shahh7107@gmail.com>
2025-11-24 15:09:57 -0500

    Changes

--- src/app/(Kambaz)/Courses/client.ts
+++ src/app/(Kambaz)/Courses/client.ts
@@ -22,31 +22,31 @@ export const createCourse = async (course: any) => {
 };
 
 export const deleteCourse = async (id: string) => {
-  const { data } = await axios.delete(`${COURSES_API}/${id}`);
+  const { data } = await axiosWithCredentials.delete(`${COURSES_API}/${id}`);
   return data;
 };
 
 export const updateCourse = async (course: any) => {
-  const { data } = await axios.put(`${COURSES_API}/${course._id}`, course);
+  const { data } = await axiosWithCredentials.put(`${COURSES_API}/${course._id}`, course);
   return data;
 };
 
 export const findModulesForCourse = async (courseId: string) => {
-  const response = await axios.get(`${COURSES_API}/${courseId}/modules`);
+  const response = await axiosWithCredentials.get(`${COURSES_API}/${courseId}/modules`);
   return response.data;
 };
 
 export const createModuleForCourse = async (courseId: string, module: any) => {
-  const response = await axios.post(`${COURSES_API}/${courseId}/modules`, module);
+  const response = await axiosWithCredentials.post(`${COURSES_API}/${courseId}/modules`, module);
   return response.data;
 };
 
 export const deleteModule = async (moduleId: string) => {
-  const response = await axios.delete(`${MODULES_API}/${moduleId}`);
+  const response = await axiosWithCredentials.delete(`${MODULES_API}/${moduleId}`);
   return response.data;
 };
 
 export const updateModule = async (module: any) => {
-  const { data } = await axios.put(`${MODULES_API}/${module._id}`, module);
+  const { data } = await axiosWithCredentials.put(`${MODULES_API}/${module._id}`, module);
   return data;
 };
